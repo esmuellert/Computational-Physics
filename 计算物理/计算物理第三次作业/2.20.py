8af3e9c15a5da27aa9c67c9cc4004aa80f92e1fd
esmuellert <myn508@hotmail.com>
2018-10-16 19:08:30 +0800

    update

--- 计算物理/计算物理第三次作业/2.20.py
+++ 计算物理/计算物理第三次作业/2.20.py
@@ -11,7 +11,7 @@ class ball(object):
         self.B2_sub_m = B2_sub_m
         self.v_0 = v_0
         self.v_x_0 = v_0 * math.cos(self.theta)
-        self.v_y_0 = v_0 * math.sin(self.theta)
+        self.v_z_0 = v_0 * math.sin(self.theta)
         self.g = g
         self.t = np.arange(0,time_duration + time_step,time_step)
         self.x = np.arange(0,time_duration + time_step,time_step)
@@ -20,7 +20,7 @@ class ball(object):
         self.v_y = np.arange(0,time_duration + time_step,time_step)
         self.v_x = np.arange(0,time_duration+time_step,time_step)
         self.v_z = np.arange(0,time_duration+time_step,time_step)
-        self.v_y[0] = self.v_y_0
+        self.v_z[0] = self.v_z_0
         self.v_x[0] = self.v_x_0
         self.time_step = time_step
         self.i = 0
@@ -28,9 +28,9 @@ class ball(object):
         for i in range(self.t.shape[0]-1):
             self.v_x[i+1] = self.v_x[i] - self.B2_sub_m*math.sqrt(self.v_x[i]**2+self.v_y[i]**2+self.v_z[i]**2)*self.v_x[i]*self.time_step
             self.x[i+1] = self.x[i] + self.v_x[i] * self.time_step
-            self.v_y[i+1] = self.v_y[i] + 0.423*math.sin(20*math.pi/180)*self.g*self.time_step - self.g * self.time_step - self.B2_sub_m*math.sqrt(self.v_x[i]**2+self.v_y[i]**2+self.v_z[i]**2)*self.v_y[i]*self.time_step
+            self.v_y[i+1] = self.v_y[i] - self.g * self.time_step - self.B2_sub_m*math.sqrt(self.v_x[i]**2+self.v_y[i]**2+self.v_z[i]**2)*self.v_y[i]*self.time_step
             self.y[i+1] = self.y[i] + self.v_y[i] * self.time_step
-            self.v_z[i+1] = self.v_z[i] + 0.423*math.cos(20*math.pi/180)*self.g*self.time_step-self.B2_sub_m*math.sqrt(self.v_x[i]**2+self.v_y[i]**2+self.v_z[i]**2)*self.v_z[i]*self.time_step
+            self.v_z[i+1] = self.v_z[i] + 0.423*self.g*self.time_step-self.B2_sub_m*math.sqrt(self.v_x[i]**2+self.v_y[i]**2+self.v_z[i]**2)*self.v_z[i]*self.time_step
             self.z[i+1] = self.z[i] + self.v_z[i]*self.time_step
 
     def plot(self):
@@ -44,7 +44,7 @@ class ball(object):
         plt.ylabel('z/m')
         plt.show()
 
-baseball = ball(v_0=40,theta=0,time_duration=1)
+baseball = ball(v_0=40,theta=20,time_duration=1)
 baseball.calculate_with_drag()
 baseball.plot()
 baseball.plot_show()
