c520c062cfccd9595f90ee56b39df2a71e7b65b2
esmuellert <myn508@hotmail.com>
2019-01-06 21:43:42 +0800

    1

---
+++ 计算物理/计算物理期末论文/1.py
@@ -0,0 +1,80 @@
+# In[1]
+
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy.constants import *
+import random
+
+class ising_model(object):
+    def __init__(self,T,iteration):
+        self.S = np.ones((10,10))
+        self.T = T
+        self.iter = iteration
+
+    def calculate_hamilton(self,S):
+        s_u = np.insert(S,0,values=0,axis=0)
+        s_u = np.delete(s_u,-1,axis=0)
+        
+        s_d = np.insert(S,10,values=0,axis=0)
+        s_d = np.delete(s_d,0,axis=0)
+        
+        s_l = np.insert(S,0,values=0,axis=1)
+        s_l = np.delete(s_l,-1,axis=1)
+
+        s_r = np.insert(S,10,values=0,axis=1)
+        s_r = np.delete(s_r,0,axis=1)        
+
+        first = (np.sum(S*s_u)+np.sum(S*s_d)+np.sum(S*s_l)+np.sum(S*s_r))
+
+        hamilton = first
+        return hamilton
+
+    def interation(self):
+        hamilton = self.calculate_hamilton(self.S)
+        self.mag = np.array([np.sum(self.S)/100])
+        for n in range(self.iter):
+            i = random.randint(0, 9)
+            j = random.randint(0, 9)
+            self.S[i][j] = -self.S[i][j]
+            hamilton_1 = self.calculate_hamilton(self.S)
+            E_flip = hamilton_1-hamilton
+
+            if E_flip<=0:
+                hamilton = hamilton_1
+                m = np.sum(self.S)/100
+                self.mag = np.append(self.mag,m)
+                continue
+            if E_flip>0:
+                r = random.random()
+                B = np.exp(-E_flip/(self.T))
+                if r<=B:
+                    hamilton = hamilton_1
+                    m = np.sum(self.S)/100
+                    self.mag = np.append(self.mag,m)
+                    continue
+                if r>B:
+                    self.S[i][j] = -self.S[i][j]
+                    m = np.sum(self.S)/100
+                    self.mag = np.append(self.mag,m)                    
+                    continue
+
+
+    def plot(self):
+        plt.figure(figsize = (10, 10))
+        plt.plot(range(self.iter+1),self.mag)
+        plt.show()                        
+
+
+
+
+
+            
+
+
+# In[2]
+ising=ising_model(T=1,iteration=1000)
+ising.interation()
+ising.plot()
+
+# In[3]
+ising.S
