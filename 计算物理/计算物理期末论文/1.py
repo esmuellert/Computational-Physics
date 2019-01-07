a2c5c70a21e210d3768af3e33101d338be24baee
esmuellert <myn508@hotmail.com>
2019-01-07 11:58:50 +0800

    save

--- 计算物理/计算物理期末论文/1.py
+++ 计算物理/计算物理期末论文/1.py
@@ -2,7 +2,6 @@
 
 import numpy as np
 import matplotlib.pyplot as plt
-from scipy.constants import *
 import random
 
 class ising_model(object):
@@ -27,23 +26,26 @@ class ising_model(object):
         first = (np.sum(S*s_u)+np.sum(S*s_d)+np.sum(S*s_l)+np.sum(S*s_r))
         second = -np.sum(S)
 
-        hamilton = first
+        hamilton = -first
         return hamilton
 
     def interation(self):
         hamilton = self.calculate_hamilton(self.S)
         self.mag = np.array([np.sum(self.S)/100])
+        self.hamilton = np.array([hamilton/100])
+
         for n in range(self.iter):
             i = random.randint(0, 9)
             j = random.randint(0, 9)
             self.S[i][j] = -self.S[i][j]
             hamilton_1 = self.calculate_hamilton(self.S)
-            E_flip = hamilton_1-hamilton
+            E_flip = (hamilton_1-hamilton)/2
 
             if E_flip<=0:
                 hamilton = hamilton_1
                 m = np.sum(self.S)/100
                 self.mag = np.append(self.mag,m)
+                self.hamilton = np.append(self.hamilton,hamilton/100)
                 continue
             if E_flip>0:
                 r = random.random()
@@ -52,16 +54,22 @@ class ising_model(object):
                     hamilton = hamilton_1
                     m = np.sum(self.S)/100
                     self.mag = np.append(self.mag,m)
+                    self.hamilton = np.append(self.hamilton,hamilton/100)
                     continue
                 if r>B:
                     self.S[i][j] = -self.S[i][j]
                     m = np.sum(self.S)/100
-                    self.mag = np.append(self.mag,m)                    
+                    self.mag = np.append(self.mag,m)
+                    self.hamilton = np.append(self.hamilton,hamilton/100)                    
                     continue
 
 
-    def plot(self):
+
+    def plot_t(self):
         plt.plot(range(self.iter+1),self.mag)
+        plt.xlabel("iteration")
+        plt.title("Mean Magetization-Time  "+"T="+str(self.T))
+        plt.ylabel("Mean Magetization")
         plt.show()                        
 
 
@@ -72,10 +80,49 @@ class ising_model(object):
 
 
 # In[2]
-ising=ising_model(T=2,iteration=1000)
-ising.interation()
-ising.plot()
-ising.mag
+for t in [1.2,2.1,2.269,4]:
+    ising=ising_model(T=t,iteration=100000)
+    ising.interation()
+    ising.plot_t()
+    plt.savefig("T="+str(t)+".jpg")
+
+
+
+
 # In[3]
-import random
-ising.calculate_hamilton(3*np.ones((10,10)))
+mean_mag = np.array([])
+temperature=np.arange(0.1,5,0.1)
+for t in temperature:
+        ising=ising_model(T=t,iteration=100000)
+        ising.interation()
+        mean_mag = np.append(mean_mag,np.mean(abs(ising.mag[30000:])))
+
+def plot():
+    plt.scatter(temperature,mean_mag)
+    plt.xlabel("temperature")
+    plt.title("Mean Magetization-Temperature Phase Graph")
+    plt.ylabel("Mean Magetization")
+    plt.show()
+    plt.savefig("Mean Magetization-Temperature Phase Graph.jpg")
+
+plot()
+
+
+# In[4]
+mean_energy = np.array([])
+temperature=np.arange(0.1,5,0.1)
+for t in temperature:
+        ising=ising_model(T=t,iteration=10000)
+        ising.interation()
+        mean_energy = np.append(mean_energy,np.mean(ising.hamilton))
+
+def plot():
+    plt.scatter(temperature,mean_energy)
+    plt.xlabel("temperature")
+    plt.title("Mean Energy-Temperature Phase Graph")
+    plt.ylabel("Mean Energy")
+    plt.savefig("Mean Energy-Temperature Phase Graph.jpg")
+    plt.show()
+
+
+plot()
